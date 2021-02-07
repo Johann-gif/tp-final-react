f473667b4c8b33845d91a5249816e1a2f06565e0
Johann-gif <55973161+Johann-gif@users.noreply.github.com>
2021-02-07 14:58:20 +0100

    Personnalisation page 404 c'est plus joli

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./styles.css";
 
 export default class ErrorPage extends React.Component {
   render() {
-    return <h1>ERROR 404 FRERE</h1>;
+    return (
+      <div class="container">
+        <div class="row">
+          <div class="xs-12 md-6 mx-auto">
+            <div id="countUp">
+              <div class="number">404</div>
+              <div class="text">Page not found</div>
+              <div class="text">This may not mean anything.</div>
+              <div class="text">
+                I'm probably working on something that has blown up.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
