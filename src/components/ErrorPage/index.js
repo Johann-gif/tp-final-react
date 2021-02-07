ba1080ce25e6faaf33f24c4df1c76f83165b1dad
Johann-gif <55973161+Johann-gif@users.noreply.github.com>
2021-02-07 15:10:12 +0100

    Retour arrière

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,20 +1,13 @@
 import React from "react";
-import "./styles.css";
 
 export default class ErrorPage extends React.Component {
   render() {
     return (
       <div class="container">
         <div class="row">
-          <div class="xs-12 md-6 mx-auto">
-            <div id="countUp">
-              <div class="number">404</div>
-              <div class="text">Page not found</div>
-              <div class="text">This may not mean anything.</div>
-              <div class="text">
-                I'm probably working on something that has blown up.
-              </div>
-            </div>
+          <div>
+            <h1>404</h1>
+            <h6>La page n'existe pas</h6>
           </div>
         </div>
       </div>
